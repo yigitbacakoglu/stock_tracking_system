a544aae6556af899b6d0d68944b36cec49c51a36
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2012-11-26 22:46:42 +0200

    mysql

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,9 @@ StockTrackingSystem::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
+  config.assets.initialize_on_precompile = false
+  config.gem "cancan"
+  
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
